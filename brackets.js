c9cafbc1155cbfd26bc81f09246a732c06309836
dexoey <m.azarboon@gmail.com>
2016-01-15 15:06:22 +0200

    first commit

---
+++ brackets.js
@@ -0,0 +1,12 @@
+{
+  "language": {
+      "javascript": {
+          "linting.prefer": ["JSHint"],
+          "linting.usePreferredOnly": true
+      }
+  },
+  "jshint.globals": {
+      "angular": true,
+      "module": true,
+      "document": true
+  },
